4c10083834f48090d633bcbdd9fe00d748628969
N Shravan Reddy <nshravanreddy6@gmail.com>
2024-08-19 15:28:03 +0530

    normalization

--- pytorch-paligrmma-live/modeling_siglip.py
+++ pytorch-paligrmma-live/modeling_siglip.py
@@ -61,8 +61,59 @@ class SiglipVisionEmbeddings(nn.Module):
         embeddings=embeddings+self.position_embedding(self.position_ids)
 
         return embeddings
+class SiglipAttention(nn.Module):
+    def __init__(self,config):
+        super().__init__()
+        self.config=config
+        self.embed_dim=config.hidden_size
+        self.num_heads=config.num_attention_heads
+        self.head_dim=self.embed_dim//self.num_heads
+        self.scale=self.head_dim ** -0.5
+        self.dropout=config.attention_dropout
 
+        self.k_proj=nn.Linear(self.embed_dim,self.embed_dim)
+        self.v_proj=nn.Linear(self.embed_dim,self.embed_dim)
+        self.q_proj=nn.Linear(self.embed_dim,self.embed_dim)
+        self.out_proj=nn.Linear(self.embed_dim,self.embed_dim)
+    def forward(self,hidden_states:torch.Tensor,) -> Tuple[Torch.Tensor,Optional[torch.Tensor]]:
+        batch_size,seq_len,_=hidden_states.size()
+        query_states=self.q_proj(hidden_states)
+        key_states=self.k_proj(hidden_states)
+        value_states=self.v_proj(hidden_states)
+        query_states=query_states.view(batch_size,seq_len,self.num_heads,self.head_dim).transpose(1,2)
+        key_states=key_states.view(batch_size,seq_len,self.num_heads,self.head_dim).transpose(1,2)
+        value_states=value_states.view(batch_size,seq_len,self.num_heads,self.head_dim).transpose(1,2)
+        attn_weights=(torch.matmul(query_states,key_states.transpose(2,3))* self.scale)
+class SiglipMLP(nn.Module):
+    def __init__(self,config):
+        super().__init__()
+        self.config=config
+        self.fc1=nn.Linear(config.hidden_size,config.intermediate_size)
+        self.fc2=nn.Linear(config.intermediate_size,config.hidden_size)
+    def forward(self, hidden_states:torch.Tensor)->torch.Tensor:
+        hidden_states=self.fc1(hidden_states)
+        hidden_states=nn.functional.gelu(hidden_states,approximate="tanh")
         
+class SiglipEncoderLayer(nn.Module):
+    def __init__(self,config: SiglipVisionConfig):
+        super().__init__()
+        self.embed_dim=config.hidden_size
+        self.self_attn=SiglipAttention(config)
+        self.layer_norm1=nn.LayerNorm(self.embed_dim,eps=config.layer_norm_eps)
+        self.mlp=SiglipMLP(config)
+        self.layer_norm2=nn.LayerNorm(self.embed_dim,eps=config.layer_norm_eps)       
+
+    def forward(self,hidden_states:torch.Tensor) -> torch.Tensor:
+        residual=hidden_states
+        hidden_states=self.layer_norm1(hidden_states)
+        hidden_states,_=self.self_attn(hidden_states=hidden_states)
+        hidden_states=residual+hidden_states
+        residual=hidden_states
+        hidden_states=self.layer_norm2(hidden_states)
+        hidden_states=self.mlp(hidden_states)
+        hidden_states=residual+hidden_states
+
+    return hidden_states
 class SiglipVisionTransformer(nn.Module):
     def __init__(self,config: SiglipVisionConfig):
         super().__init__()
@@ -91,5 +142,4 @@ class SiglipVisionModel(nn.Module):
         return self.vision_model(pixel_values=pixel_values)
 
 
-
-55 min 
+1.58 min
